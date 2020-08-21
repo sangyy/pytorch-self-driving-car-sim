9016223c95d9f1e0e0f28eaa28ba54f48a529456
conda act pt <vscode@163.com>
2020-08-21 20:37:11 +0800

    drive program

--- Test.py
+++ Test.py
@@ -1,16 +1,17 @@
 print('Setting UP')
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import socketio
 import eventlet
 import numpy as np
 from flask import Flask
-from tensorflow.keras.models import load_model
+# from tensorflow.keras.models import load_model
 import base64
 from io import BytesIO
 from PIL import Image
 import cv2
- 
+import torch
+from torch.autograd import Variable
 #### FOR REAL TIME COMMUNICATION BETWEEN CLIENT AND SERVER
 sio = socketio.Server()
 #### FLASK IS A MICRO WEB FRAMEWORK WRITTEN IN PYTHON
@@ -22,7 +23,7 @@ maxSpeed = 10
 def preProcess(img):
     img = img[60:135,:,:]
     img = cv2.cvtColor(img, cv2.COLOR_RGB2YUV)
-    img = cv2.GaussianBlur(img,  (3, 3), 0)
+    # img = cv2.GaussianBlur(img,  (3, 3), 0)
     img = cv2.resize(img, (200, 66))
     img = img/255
     return img
@@ -34,8 +35,17 @@ def telemetry(sid, data):
     image = Image.open(BytesIO(base64.b64decode(data['image'])))
     image = np.asarray(image)
     image = preProcess(image)
-    image = np.array([image])
-    steering = float(model.predict(image))
+    # image = np.array([image])
+    # steering = float(model.predict(image))
+    image = torch.Tensor(image)
+    # print(image.shape)
+    image = image.view(1, 3, 66, 200)
+    image = Variable(image)
+    # print(image.shape)
+    steering = model(image).view(-1).data.numpy()[0]
+
+
+
     throttle = 1.0 - speed / maxSpeed
     print(f'{steering}, {throttle}, {speed}')
     sendControl(steering, throttle)
@@ -55,7 +65,9 @@ def sendControl(steering, throttle):
  
  
 if __name__ == '__main__':
-    model = load_model('model.h5')
+    # model = torch.load('model.h5')
+    checkpoint = torch.load('model.h5', map_location=lambda storage, loc: storage)
+    model = checkpoint['model']
     app = socketio.Middleware(sio, app)
     ### LISTEN TO PORT 4567
     eventlet.wsgi.server(eventlet.listen(('', 4567)), app)
